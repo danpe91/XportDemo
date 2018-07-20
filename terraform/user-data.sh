04e3205e34c072c9218c20bd90fee753d1a497da
Daniel Pedroza <danpe91@gmail.com>
2018-07-20 01:32:40 -0500

    Include installation of DataDog agent in EC2 instances

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -6,3 +6,5 @@ rpm -U https://download-i2.fedoraproject.org/pub/epel/epel-release-latest-7.noar
 yum install nginx -y
 systemctl enable nxinx
 systemctl start nginx
+
+DD_API_KEY=641a88a25f2f2a4f882d1dde189e5eb5 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
